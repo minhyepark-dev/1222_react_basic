c904c4f58d72a0898e41c90c29f1eb9ea9e2c57e
minhyepark <minhyepark.dev@gmail.com>
2021-12-22 11:22:10 +0900

    useState를 활용하여 버튼 클릭시 변경된 이름으로 재렌더링

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import './css/style.css';
-import { Logo, Menu } from './components/Header.js';
-import Panel from './components/Panel.js';
-import Footer from './components/Footer.js';
-
-
+import "./css/style.css";
+import { Logo, Menu } from "./components/Header.js";
+import Panel from "./components/Panel.js";
+import Footer from "./components/Footer.js";
 
 function App() {
-	return (
-		<div className="App">
-			{/* 전체 레이아웃 */}
-			<figure>
-				<Logo />
-				<Menu />
-
-				<section>
-					<Panel />
-				</section>
-
-				<Footer />
-			</figure>
-		</div>
-	);
+    return (
+        <div className="App">
+            {/* 전체 레이아웃 */}
+            <figure>
+                <Logo />
+                <Menu />
+                <Panel />
+                <Footer />
+            </figure>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,37 +1,42 @@
+import { useState } from "react";
+
 function Panel() {
-	const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
-	const path = process.env.PUBLIC_URL;
-	const deg = 360/arr.length;	
-	const btnStyle = {position: 'fixed', top:0, left:0}
-	
-	function changeArr(txt){
-		arr[0] = txt;
-		console.log(arr);
-	}
+    const arr = ["Blizzards", "Calm", "Dusty_Road", "Escape", "Payday", "Retreat", "Seasonal", "Vespers"];
+    const path = process.env.PUBLIC_URL;
+    const deg = 360 / arr.length;
+    const btnStyle = { position: "fixed", top: 0, left: 0 };
 
-	return (
-		<>
-		<button style={btnStyle} onClick={()=>changeArr('Escape')}>button</button>
-		{
-			arr.map((data,index)=>{	
-				let style = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}	
-				let imgSrc = `${path}/img/${data}.jpg`
+    const [names, setNames] = useState(arr);
 
-				return (
-					<article key={index} style={style}>
-						<div className="inner">
-							<div className="pic">
-								<img src={imgSrc} />
-							</div>
+    const changeState = () => {
+        let newArr = [...arr];
+        newArr[0] = "Escape";
+        setNames(newArr);
+    };
 
-							<h2>{data}</h2>				
-						</div>
-					</article>
-				)
-			})
-		}
-		</>		
-	)
+    return (
+        <>
+            <button style={btnStyle} onClick={changeState}>
+                button
+            </button>
+            <section>
+                {names.map((data, index) => {
+                    let style = { transform: `rotate(${deg * index}deg) translateY(-100vh)` };
+                    let imgSrc = `${path}/img/${data}.jpg`;
+                    return (
+                        <article key={index} style={style}>
+                            <div className="inner">
+                                <div className="pic">
+                                    <img src={imgSrc} />
+                                </div>
+                                <h2>{data}</h2>
+                            </div>
+                        </article>
+                    );
+                })}
+            </section>
+        </>
+    );
 }
 
 export default Panel;
